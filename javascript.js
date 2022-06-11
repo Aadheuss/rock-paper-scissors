7920e14ba205b41daeb2008865900a412cfc7386
Aadheuss <aadheusx@gmail.com>
2022-06-11 17:52:55 +0700

    Edit javascript.js and add a new function
    i add separate score variable to store the score of player and computer,
    and I edit the singleRound function to return more informative string
    and add one score for the winner of the round. i also add a new function
    game() to loop the singleRound five time and announce who the winner is
    when the loop ended.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
-//ask for player input
 //a player play against computer
-//computer randomly return either 'Rock', 'Paper'. or 'Scissors'
+let playerScore = 0;
+let computerScore = 0;
+//score variable track the starting score point
 function computerPlay() {
   let handSelection = ['rock', 'paper', 'scissors'];
   return handSelection[Math.floor(Math.random()*handSelection.length)];
 };
-
+//computer randomly return either 'Rock', 'Paper'. or 'Scissors'
 let computerSelection = computerPlay();
 //return random selection between rock, paper, or scissors.
 function playerChoice(string) {
@@ -21,37 +22,64 @@ function playerChoice(string) {
     return 'No selection is chosen';
   }
 }
-//make player selection between Rock, Paper, or scissors
+
 function singleRound(playerSelection, computerSelection) {
-//rock beats paper
+  playerSelection = prompt ('Choose your hand!', '');
+//ask for player input
+//make player selection between Rock, Paper, or scissors
+  playerSelection = playerSelection.toString();
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerPlay();
   if ((playerSelection == 'paper') && (computerSelection == 'scissors')) {
-    return 'You Lose! Scissors beats Paper';
+    return 'You Lose! Scissors beats Paper, your Score is' + ' ' + playerScore + ' ' + 'your opponent\'s score is' + 
+    ' ' + (computerScore += 1);
   }else if ((playerSelection == 'scissors') && (computerSelection == 'rock')) {
-    return 'You Lose! Rock beats Scissors';
+    return 'You Lose! Rock beats Scissors, your Score is' + ' ' + playerScore + ' ' + 'your opponent\'s score is' + 
+    ' ' + (computerScore += 1);
   }else if ((playerSelection == 'rock') && (computerSelection == 'paper')) {
-    return 'You Lose! Paper beats rock';
+    return 'You Lose! Paper beats rock, your Score is' + ' ' + playerScore + ' ' + 'your opponent\'s score is' + 
+    ' ' + (computerScore += 1);
   }else if ((playerSelection == 'scissors') && (computerSelection == 'paper')) {
-    return 'You Win! Scissors beats Paper';
+    return 'You Win! Scissors beats Paper, your Score is' + ' ' + (playerScore += 1) + ' ' + 'your opponent\'s score is' 
+    + ' ' + computerScore;
   }else if ((playerSelection == 'paper') && (computerSelection == 'rock')) {
-    return 'You Win! Paper beats Scissors';
+    return 'You Win! Paper beats Scissors, your Score is' + ' ' + (playerScore += 1) + ' ' + 'your opponent\'s score is' 
+    + ' ' + computerScore;
   }else if ((playerSelection == 'rock') && (computerSelection == 'scissors')) {
-    return 'You Win! Rock beats Scissors';
+    return 'You Win! Rock beats Scissors, your Score is' + ' ' + (playerScore += 1) + ' ' + 'your opponent\'s score is' 
+    + ' ' + computerScore;
   }else if ((playerSelection == 'rock') && (computerSelection == 'rock') || 
              (playerSelection == 'paper') && (computerSelection == 'paper') ||
              (playerSelection == 'scissors') && (computerSelection == 'scissors')) {
-    return 'It\'s a draw!';
+    return 'It\'s a draw!, your Score is' + ' ' + playerScore + ' ' + 'your opponent\'s score is' + 
+    ' ' + computerScore;
   }else {
     return 'You haven\'t chosen your hand!';
   }
-/*'rock' > 'scissors' == true;
-'paper' > 'rock' == true;
-'scissors' > 'paper' == true;*/
+//rock' > 'scissors' == true;
+//'paper' > 'rock' == true;
+//'scissors' > 'paper' == true;
 
 }
 //play a single round of Rock Paper Scissors
 //make the selection case-insensitive
 //declare the winner of the round when it finished
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let result = singleRound();
+    console.log(result);
+    console.log(playerScore > computerScore);
+//call the playRound function
 //play 5 round of game and keep score
-//report a winner
+    if (i == 4) {
+      if (playerScore > computerScore) {
+      return ('Congratulations!! You Win!');
+      }else if (playerScore == computerScore) {
+      return ('It\'s a Draw! Better luck next time!');
+      }else {
+      return('You Lose! The Computer will rule over the World!');
+      }
+    }   
+  }
+}
+//report a winner or loser at the end
