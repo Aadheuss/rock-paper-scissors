7bbc1423b6899087276c7b7f02dc9c526b946181
Aadheuss <aadheusx@gmail.com>
2022-06-19 02:36:53 +0700

    Make a new function to change color of selection
    I add a new function to change the button color if it's selected by the
    either the player or the computer.

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ const selectionButtons = document.querySelectorAll(`[data-selection]`);
 const finalColumn = document.querySelector(`[data-final-column]`);
 const computerScore = document.querySelector(`[data-computer-score]`)
 const playerScore = document.querySelector(`[data-player-score]`);
+const pButtons = document.querySelectorAll(`button.selection`);
 const SELECTIONS = [
   {
     name: 'rock',
@@ -17,14 +18,29 @@ const SELECTIONS = [
   }
 ];
 
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('chosen', 'pick');
+}
+
+const buttons = Array.from(document.querySelectorAll('button'));
+  buttons.forEach(button => button.addEventListener('transitionend', removeTransition));
+//remove the border change
 function incrementScore(score) {
   score.innerText = parseInt(score.innerText) + 1; 
 }
+//keep track of the score
 function randomSelection() {
   const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
   return SELECTIONS[randomIndex];
 };
 
+pButtons.forEach(button => button.addEventListener('click', () => {
+  const cButton = document.querySelector(`.${button.className}`);
+  button.classList.add('pick');
+}))
+
+//computer's random selection
 selectionButtons.forEach(selectionButton => {
   selectionButton.addEventListener('click', e => {
     const selectionName = selectionButton.dataset.selection;
@@ -32,9 +48,11 @@ selectionButtons.forEach(selectionButton => {
     makeSelection(selection)
   })
 })
+//return the value of player selection 
 
 function makeSelection(selection) {
   const computerSelection = randomSelection();
+  changeColor(computerSelection);
   const yourWinner = isWinner(selection, computerSelection);
   const computerWinner = isWinner(computerSelection, selection);
   addSelectionResult(computerSelection, computerWinner);
@@ -44,6 +62,14 @@ function makeSelection(selection) {
   if (computerWinner) incrementScore(computerScore);
 }
 
+function changeColor(changeMe) {
+//make a function that will change the button color if it's selected
+//if the button is chosen change the color of the button
+  changeMe = `.${changeMe.name}` ;
+  const cButton = document.querySelector(changeMe);
+  cButton.classList.add('chosen');
+  }
+//change the border color by adding class
 function addSelectionResult(selection, winner) {
   const div = document.createElement('div');
   div.innerText = selection.name;
@@ -51,6 +77,7 @@ function addSelectionResult(selection, winner) {
   if (winner) div.classList.add('winner');
   finalColumn.after(div);
 }
+//Track the result for the round
 
 function isWinner(selection, opponentSelection) {
   return selection.beats === opponentSelection.name; 
